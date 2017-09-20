b802bde173255bbc3f84f83dea9d1d55704d042c
William's Ernesto Limonchi Sandoval <wels.28.94@gmail.com>
2017-09-19 22:44:04 -0500

    Tutorial 2 - Boton con Arduino

---
+++ Boton-Tutorial_2/Boton-Tutorial_2.ino
@@ -0,0 +1,21 @@
+
+int led = 8; // Led -> Pin 8 del Arduino
+int boton = 2; // Boton -> Pin 2 del Arduino
+
+int estado; // Variable donde almacenamos el valor de Boton
+
+void setup(){
+  pinMode(led,OUTPUT); // Led -> Salida
+  pinMode(boton,INPUT); // Boton -> Entrada
+  }
+
+void loop(){
+  estado = digitalRead(boton); //estado -> Boton ( 1 o 0)
+
+  if (estado == HIGH){// BOTON = 1
+    digitalWrite(led,HIGH); // LED = 1
+    }
+  else{
+    digitalWrite(led,LOW); // LED = 0 
+    }
+  }
