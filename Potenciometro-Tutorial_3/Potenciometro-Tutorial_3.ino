f82892f9fd27c81bae79a9dd8868306b88ce1812
William's Ernesto Limonchi Sandoval <wels.28.94@gmail.com>
2017-09-19 22:47:39 -0500

    Tutorial 3 - Potenciometro con Arduino

---
+++ Potenciometro-Tutorial_3/Potenciometro-Tutorial_3.ino
@@ -0,0 +1,15 @@
+int Pot = 0; // Potenciometro = Pin A0 del Arduino
+int led = 3; // LEd = Pin 3 del Arduino (~) 
+
+int valor; // Variable donde almacenamos el valor obtenido del Pot
+
+void setup(){
+  pinMode(led,OUTPUT); //LED -> SALIDA
+  }
+
+void loop(){
+  valor = analogRead(Pot)/4; // Pot -> valor (Analogica) 
+                            // 0 - 1023 /4 -> 0 - 255
+  analogWrite(led,valor);    //Aumentar la intensidad segun valor
+  }  
+
